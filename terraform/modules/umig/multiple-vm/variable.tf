c3b3fe88cbbe3f831b5c63763b5eca6b1902ba41
summu97 <135579543+summu97@users.noreply.github.com>
2024-04-27 13:54:49 +0530

    Create variable.tf

---
+++ terraform/modules/umig/multiple-vm/variable.tf
@@ -0,0 +1,14 @@
+variable "machine_type" {
+type = string
+default = "n2-standard-2"
+}
+
+variable "zone" {
+type = string
+default = "us-central1-a"
+}
+
+variable "image" {
+type = string
+default = "debian-cloud/debian-11"
+}
