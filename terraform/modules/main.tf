500a81da9c452c54f1bcc6fd70031ece546c8936
summu97 <135579543+summu97@users.noreply.github.com>
2024-04-27 11:46:31 +0530

    Update main.tf

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -1,6 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4"
+    }
+  }
+}
+
 provider "google" {
   project     = "ringed-hallway-417305"
   region      = "us-east1-b"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "adq-bucket-getteam"
+    prefix  = "terraform/state"
+  }
 }
 
 module "bastion-host" {
